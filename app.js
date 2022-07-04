92b9ec37213c866cdb97a5a23821f05ce7d7ea1c
nguyendoanduylinh <nguyen.doan.duy.linh@gmail.com>
2022-07-04 15:37:31 +0700

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saterday",
+];
+let date = new Date();
+let index = date.getDay();
+
+console.log(weekdays[index]);
